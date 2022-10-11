ade07c8ea58f958f5c4bfd932d4c38ef0c8b5d4e
Michael Lesser <mlesser@arizona.edu>
2022-10-11 06:07:20 -0700

    linux

--- support/90prime.py
+++ support/90prime.py
@@ -3,22 +3,27 @@
 import os
 
 if os.name == "posix":
-    pass
-    # AZCAM_DATAROOT="/home/lesser/data"
-    # export AZCAM_DATAROOT
-    # echo AzCam data root is $AZCAM_DATAROOT
-    # echo Activating azcam virtual environment
+    AZCAM_DATAROOT="/home/lesser/data"
+    os.environ['AZCAM_DATAROOT']=AZCAM_DATAROOT
+
+    print(f'AzCam data root is {AZCAM_DATAROOT}')
+    print('Activating azcam virtual environment')
+
     # source ~/azcam/venvs/azcam/bin/activate
     # ipython --profile azcamserver -i -c "import azcam_90prime.server; from azcam.cli import *"
+
+    command = ". ~/azcam/venvs/azcam/bin/activate & ipython --profile azcamserver -i -c \"import azcam_90prime.server\""
+    #command = ". /home/lesser/azcam/venvs/azcam/bin/activate"
+    os.system(command)
 
 else:
     AZCAM_DATAROOT='/data'
     os.environ['AZCAM_DATAROOT']=AZCAM_DATAROOT
+
     AZCAM_AZCAMTOOL='C:\\azcam\\azcam-tool\\azcam_tool\\builds\\azcamtool.exe'
     os.environ['AZCAM_AZCAMTOOL']=AZCAM_AZCAMTOOL
 
     print(f'AzCam data root is {AZCAM_DATAROOT}')
-
     print('Activating azcam virtual environment')
 
     command = "\\data\\venvs\\azcam\\Scripts\\activate.bat & ipython --profile azcamserver -i -c \"import azcam_90prime.server\""
