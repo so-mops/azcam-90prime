7db789d7425c96ea42ceb2eb3a5b3267c10ad395
Michael Lesser <mlesser@arizona.edu>
2023-08-08 10:06:54 -0700

    amp_cfg

--- azcam_90prime/detector_bok90prime.py
+++ azcam_90prime/detector_bok90prime.py
@@ -7,7 +7,7 @@ detector_bok90prime = {
     "description": "90prime mosaic",
     "ref_pixel": [4091.04, 4277.99],
     "format": [4032 * 2, 6, 0, 20, 4096 * 2, 0, 0, 0, 0],
-    "focalplane": [2, 2, 4, 4, "3210321001230123"],
+    "focalplane": [2, 2, 4, 4, [3,2,1,0,3,2,1,0,0,1,2,3,0,1,2,3],
     "roi": [1, 4032 * 2, 1, 4096 * 2, 1, 1],
     "ext_position": [
         [2, 2],
@@ -91,88 +91,10 @@ detector_bok90prime_one = {
     "description": "90prime mosaic in single chip mode",
     "ref_pixel": [2016.0, 2048.0],
     "format": [4032, 6, 0, 20, 4096, 0, 0, 0, 0],
-    "focalplane": [1, 1, 2, 2, "0123"],
+    "focalplane": [1, 1, 2, 2, [0,1,2,3],
     "roi": [1, 4032, 1, 4096, 1, 1],
     "ext_position": [[1, 1], [2, 1], [1, 2], [2, 2]],
     "jpg_order": [1, 2, 3, 4],
-}
-
-detector_bok90prime_6k = {
-    "name": "bok90prime_6k",
-    "description": "6k CCD",
-    "ref_pixel": [3060.0, 3060.0],
-    "format": [6120, 0, 0, 20, 6120, 0, 0, 0, 0],
-    "focalplane": [1, 1, 2, 2, "0123"],
-    "roi": [1, 6120, 1, 6120, 1, 1],
-    "ext_position": [[1, 1], [2, 1], [1, 2], [2, 2]],
-    "jpg_order": [1, 2, 3, 4],
-    "amp_cfg": [0, 1, 2, 3],
-    "ext_number": [1, 2, 3, 4],
-    "det_number": [1, 1, 1, 1],
-    "det_position": [
-        [1, 1],
-        [1, 1],
-        [1, 1],
-        [1, 1],
-    ],
-    "det_gap": [
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-    ],
-    "amp_position": [
-        [1, 1],
-        [2, 1],
-        [1, 2],
-        [2, 2],
-    ],
-    "amp_pixel_position": [
-        [1, 1],
-        [6120, 1],
-        [1, 6120],
-        [6120, 1],
-    ],
-    "ext_name": [
-        "im1",
-        "im2",
-        "im3",
-        "im4",
-    ],
-}
-
-detector_bok90prime_6k_2amp = {
-    "name": "bok90prime_6k_2amp",
-    "description": "6k CCD",
-    "ref_pixel": [3060.0, 3060.0],
-    "format": [6120, 4, 0, 20, 6120, 0, 0, 0, 0],
-    "focalplane": [1, 1, 2, 1, "01"],
-    "roi": [1, 6120, 1, 6120, 1, 1],
-    "ext_position": [[1, 1], [2, 1]],
-    "jpg_order": [1, 2],
-    "amp_cfg": [0, 1],
-    "ext_number": [1, 2],
-    "det_number": [1, 1],
-    "det_position": [
-        [1, 1],
-        [1, 1],
-    ],
-    "det_gap": [
-        [0.0, 0.0],
-        [0.0, 0.0],
-    ],
-    "amp_position": [
-        [1, 1],
-        [2, 1],
-    ],
-    "amp_pixel_position": [
-        [1, 1],
-        [6120, 1],
-    ],
-    "ext_name": [
-        "im1",
-        "im2",
-    ],
 }
 
 gapX_new = 2.959/.015  # gaps between amplifiers and CCDs (in pixels)
